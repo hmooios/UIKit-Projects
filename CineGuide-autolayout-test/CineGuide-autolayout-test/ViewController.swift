689970b69d5ae7fcff89da7b091db4be06b89828
hmooios <135945583+hmooios@users.noreply.github.com>
2023-12-16 17:14:12 +0630

    Auto Layout test

---
+++ CineGuide-autolayout-test/CineGuide-autolayout-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineGuide-autolayout-test
+//
+//  Created by Hmoo Myat Theingi on 16/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

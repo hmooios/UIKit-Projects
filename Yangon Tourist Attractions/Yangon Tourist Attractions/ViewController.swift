efdd425c9a8221b9c1c711b24f7b2b7e821d33a9
Hmoo <135945583+hmooios@users.noreply.github.com>
2023-12-21 21:19:25 +0630

    Complete

---
+++ Yangon Tourist Attractions/Yangon Tourist Attractions/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Yangon Tourist Attractions
+//
+//  Created by Hmoo Myat Theingi on 21/12/2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Tourist Attractions"
+        navigationController?.navigationBar.prefersLargeTitles=true
+       
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+           if item.hasPrefix("yangon"){
+               pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController{
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+
+
+}
+

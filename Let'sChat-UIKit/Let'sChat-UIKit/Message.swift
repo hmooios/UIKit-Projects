9f06d2bf1a131eb726b43cb35802e0d81bbecb04
Hmoo <135945583+hmooios@users.noreply.github.com>
2024-01-02 21:13:48 +0630

    Complete chat app

---
+++ Let'sChat-UIKit/Let'sChat-UIKit/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Let'sChat-UIKit
+//
+//  Created by Hmoo Myat Theingi on 31/12/2023.
+//
+
+import Foundation
+struct Message{
+    var sender:String
+    var body:String
+}
